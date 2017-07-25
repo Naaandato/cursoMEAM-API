6b7244d3114c3528fbeb11e7a0e7ffd10f9eebdc
hivetry <hivetry@gmail.com>
2017-07-25 14:55:19 +0200

    headers

--- app.js
+++ app.js
@@ -14,12 +14,18 @@ app.use(bodyParser.urlencoded({limit: '5mb', extended:false}));
 
 //configurar cabeceras http
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Headers', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    //res.header('Access-Control-Allow-Origin', '*');
+    //res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    //res.header('Access-Control-Allow-Headers', 'GET, POST, OPTIONS, PUT, DELETE');
+    //res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
     next();
+
 });
 
 //rutas base
